61253899886e1ec3224fb3923c243b0c786f5c93
Mahmoud Hossam Elden Ibrahim <mahmoudhossam.std@cis.asu.edu.eg>
2022-03-01 01:43:01 +0200

    first commit

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>Volta CRM</strong>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/sign-up" class="button is-success"><strong>Sign Up</strong></router-link>
+                        <router-link to="/login" class="button is-light"><strong>Login</strong></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: 'Navbar'
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
+import stpre from '../store'
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/LoginView.vue'
+import Dashboard from '../views/dashboard/DashboardView.vue'
+import MyAccount from '../views/dashboard/MyAccountView.vue'
 
 const routes = [
   {
@@ -8,18 +13,56 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      requireLogin:true,
+    }
+  },
+  {
+    path: '/dashboard/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin:true,
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUpView.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+
+})
+
+router.beforeEach((to,from,next)=> {
+  if(to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated){
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
